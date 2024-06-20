c3c48ff3cb080f6f35d73eee9bbdcb2383404fc3
Markus2332 <azim2332@gmail.com>
2024-06-20 10:28:46 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -12,8 +12,8 @@ session_start();
     <!-- Font Awesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
-<body id="head">
-<header>
+<body>
+<header id="head">
     <nav class="navbar navbar-expand-lg bg-secondary fixed-top" id="mainNav">
         <div class="container"><a class="navbar-brand js-scroll-trigger" href="#page-top">ChefMate</a>
             <button class="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i class="fas fa-bars"></i></button>
@@ -34,7 +34,7 @@ session_start();
 </header>
 
     <style>
-    #header .header{
+    #header {
         margin-bottom: 3rem !important;
         
     }
