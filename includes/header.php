06382b28399c79ff0fea1cc8598376a63868bcff
Markus2332 <azim2332@gmail.com>
2024-06-23 02:12:02 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,15 +6,15 @@ require_once __DIR__ . '/functions.php';
 <html>
 <head>
     <title>My Recipes</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
     <!-- Add Bootstrap CSS for styling -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <!-- Font Awesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
-<header id="head">
-      <nav class="navbar navbar-expand-lg bg-secondary fixed-top" id="mainNav">
+<header>
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top" id="mainNav">
         <div class="container"><a class="navbar-brand js-scroll-trigger" href="/#page-top">ChefMate</a>
             <button class="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i class="fas fa-bars"></i></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
@@ -22,22 +22,16 @@ require_once __DIR__ . '/functions.php';
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/#portfolio">RECIPES FOR YOU</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/#about">ABOUT CHEFMATE</a></li>
                     <?php if (is_logged_in()): ?>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/profile_update.php">UPDATE PROFILE</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/logout.php">LOG OUT</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/dashboard.php">DASHBOARD</a></li>
                     <?php else: ?>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/register.php">REGISTER</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/login.php">LOG IN</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/password_reset_request.php">RESET PASSWORD</a></li>
                     <?php endif; ?>
                 </ul>
             </div>
         </div>
     </nav>
 </header>
-
-
-    <style>
-    #head {
-        margin-bottom: 13rem !important;
-        
-    }
-    </style>
