5e940aa75f18281360d68583ea1aff4d51e40093
maiezra <maiez@mta.ac.il>
2024-06-25 17:33:55 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -41,9 +41,9 @@ require_once __DIR__ . '/functions.php';
     margin-top: 9rem !important;
     }
     #maninNav{
-        backgroound-color: #375755
-        color: #ffff
-        font- family: "SB Heading", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, 
+        backgroound-color: #375755;
+        color: #ffff;
+        font-family: "SB Heading", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, 
             "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
             "Segoe UI Symbol", "Noto Color Emoji";
         font-weight: 700;
